bb2d946a2e7b57b0bdc3cc93a92066dbf3900fbf
sineka-mediwave <sineka@mindwaveventures.com>
2023-09-14 15:04:43 +0530

    html form design

---
+++ README.md
@@ -0,0 +1,5 @@
+# Diary App
+
+Diary app used to add notes by adding datas in the form.
+
+## creating form in html
---
+++ app/script.js
@@ -0,0 +1,6 @@
+//Inialize the global vaiable
+const cardBody = document.querySelector("#diary-card");
+
+let myDiary = [];
+
+console.log("print");
